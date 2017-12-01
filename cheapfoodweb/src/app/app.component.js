be932aea69cea8e36fef376b23f7078f62be2669
tmalik6 <arkta2k@gmail.com>
2017-12-01 23:11:51 +0100

    separation of concern in angular

--- cheapfoodweb/src/app/app.component.js
+++ cheapfoodweb/src/app/app.component.js
@@ -6,7 +6,7 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
 var core_1 = require("@angular/core");
-var foodSubmitService_1 = require("./foodsubmit/foodSubmitService");
+var foodSubmitService_1 = require("./shared/foodSubmitService");
 var AppComponent = (function () {
     function AppComponent() {
     }
